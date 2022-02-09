53f9b9949e2d2d84a715a9e372dccbe54650efce
Robert Rondon <robrondon11@gmail.com>
2022-02-09 18:12:54 -0300

    passing joke to text to speech API, and finishing project

--- script.js
+++ script.js
@@ -105,20 +105,25 @@ const VoiceRSS = {
   },
 };
 
-// const test = function () {
-//   VoiceRSS.speech({
-//     key: 'd009cc41099a4de785efee92f2e2980b',
-//     src: 'How you doing?',
-//     hl: 'en-us',
-//     v: 'Linda',
-//     r: 0,
-//     c: 'mp3',
-//     f: '44khz_16bit_stereo',
-//     ssml: false,
-//   });
-// };
+// Disable / Enable button
+const toggleButton = function () {
+  button.disabled = !button.disabled;
+};
 
-// test();
+// Passing Joke to VoiceRSS API
+const tellMe = function (joke) {
+  VoiceRSS.speech({
+    key: 'd009cc41099a4de785efee92f2e2980b',
+    src: joke,
+    hl: 'en-us',
+    v: 'Mike',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+  console.log(joke);
+};
 
 // Get Jokes from Joke API
 const getJokes = async function () {
@@ -132,10 +137,17 @@ const getJokes = async function () {
     } else {
       joke = data.joke;
     }
-    console.log('joke ->', joke);
+
+    // Text to Speech
+    tellMe(joke);
+
+    // Disable button
+    toggleButton();
   } catch (error) {
     console.error('Opps!!', error);
   }
 };
 
-getJokes();
+//Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
